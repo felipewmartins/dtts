4ebece21e23933e6db63caf76a576aedc87ef32b
eprogramming <00hf11@gmail.com>
2014-04-13 23:48:58 -0300

    learning again...

--- src/main/groovy/Sample.groovy
+++ src/main/groovy/Sample.groovy
@@ -1,5 +1,21 @@
-print "1"
-    print "2"
-        print "3"
-            print "4"
-                print "5"
+println "### welcome to the heavy metal no description groovy tutorial ###"
+println "Download groovy"
+println "Create a GROOVY_HOME"
+for (i in 0..3) { print i }
+println "\n"
+println "Try to remember shift + alt + x + g from eclipse to run this"
+0.upto(3) { println "$it" }
+println "Problems with autocomplete on eclipse"
+2.times { print "$it-" }
+print "\n"
+0.step(21, 3) { print "$it " }
+print "\n"
+0.step(100, 5) { print "$it " }
+println ""
+println "Show information about git:"
+println "git status".execute().text
+println "Wow ! Let's try some more...Who i am ?"
+println "git config user.name".execute().text
+
+
+
